74ef8d5ed75473fc1d00714b308c76ae30f2d2b1
adanlessossi <adanlessossi@hotmail.com>
2019-03-08 15:56:08 +0100

    Added the Order and Checkout components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<Layout>
-					<BurgerBuilder />
+					<Switch>
+						<Route to='/' exact component={BurgerBuilder} />
+						<Route to='/checkout' component={Checkout} />
+					</Switch>
 				</Layout>
 			</div>
 		);
